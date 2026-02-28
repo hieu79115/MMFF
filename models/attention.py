a9b85307995fdc94d8c1b1824fb9c5dd040c1343
hieu79115 <npt911@gmail.com>
2026-02-28 16:25:12 +0700

    fix attention

--- models/attention.py
+++ models/attention.py
@@ -14,14 +14,18 @@ class CrossModalAttention(nn.Module):
         self.key_conv = nn.Conv2d(skel_channels, inter_channels, kernel_size=1)
         self.value_conv = nn.Conv2d(skel_channels, rgb_channels, kernel_size=1)
         
+        self.scale = inter_channels ** -0.5
         self.softmax = nn.Softmax(dim=-1)
+
+        self.attn_dropout = nn.Dropout(dropout)
+
         self.gamma = nn.Parameter(torch.zeros(1))
 
     def forward(self, x_rgb, x_skel):
         B, C_r, H, W = x_rgb.size()
         
         # Average Pool theo thời gian T
-        x_skel_pool = F.adaptive_avg_pool2d(x_skel, (x_skel.size(3), 1)) 
+        x_skel_pool = F.adaptive_avg_pool2d(x_skel, (1, x_skel.size(3))) 
         
         proj_query = self.query_conv(x_rgb).view(B, -1, H*W).permute(0, 2, 1)
         proj_key = self.key_conv(x_skel_pool).view(B, -1, x_skel.size(3))
